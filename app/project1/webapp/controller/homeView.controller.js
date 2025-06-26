a5ebf0c529c5947e6d1a537af4f78b09a1d85b6e
anh tuan <annguyen@abeam.com>
2025-06-26 08:01:36 +0000

    list view

--- app/project1/webapp/controller/homeView.controller.js
+++ app/project1/webapp/controller/homeView.controller.js
@@ -1,10 +1,22 @@
+// const { model } = require("@sap/cds");
+
 sap.ui.define([
-    "sap/ui/core/mvc/Controller"
-], (Controller) => {
+    "sap/ui/core/mvc/Controller",
+    "./../model/models",
+    "./../model/formatter",
+], (Controller, Model,formatter) => {
     "use strict";
 
     return Controller.extend("project1.controller.homeView", {
-        onInit() {
+        formatter: formatter,
+        onInit: async function () {
+            const oView = this.getView();
+            Model.setInitialModel(oView);
+            await Model.getListRole(oView);
+            await Model.getListDepart(oView);
+            Model.setVisibleControl(oView);
+            await Model.getEmployees(oView);            
         }
+        
     });
 });
--- app/project1/webapp/model/models.js
+++ app/project1/webapp/model/models.js
@@ -1,3 +1,5 @@
+// const { sentinelInterval } = require("@sap/cds/lib/srv/srv-models");
+
 sap.ui.define([
     "sap/ui/model/json/JSONModel",
     "sap/ui/Device"
@@ -14,7 +16,83 @@ function (JSONModel, Device) {
             var oModel = new JSONModel(Device);
             oModel.setDefaultBindingMode("OneWay");
             return oModel;
-        }
+        },
+        _setModel: function (oView, oData, sModelName) {
+            oView.setModel(new JSONModel(oData), sModelName);
+          },
+        _getModel: function (oView, sModelName) {
+            let oModel = oView.getModel(sModelName);
+            return oModel;
+          },
+        setInitialModel: function(oView){   
+        },
+        setVisibleControl: function(oView){
+            const visibleView = {
+                list: true,
+                detail: false
+            };
+            this._setModel(oView, visibleView , "VisibleControl" );
+        },
+        getListRole: async function(oView){
+            try {
+                const oResRole = await fetch("/ojt/Roles",
+                {
+                    method: "GET",
+                    headers: {
+                        "Content-Type": "application/json" }
+                });
+                if ( oResRole.status == 200 ) {
+                    let oresRoleJson = await oResRole.json();
+                    let roleList = oresRoleJson.value;
+                    this._setModel(oView, roleList, "roleList");
+                    console.log(oView.getModel("roleList"));
+                    return roleList;
+                }
+            } 
+            catch (error) {
+                return null;  
+            }    
+        },
+        getListDepart: async function(oView){
+            try {
+                const oResDe = await fetch("/ojt/Departments",
+                {
+                    method: "GET",
+                    headers: {
+                        "Content-Type": "application/json" }
+                });
+                if ( oResDe.status == 200 ) {
+                    let oresDeJson = await oResDe.json();
+                    let deList = oresDeJson.value;
+                    this._setModel(oView, deList, "departmentsList");
+                    console.log(oView.getModel("departmentsList"));
+                    return deList;
+                }
+            } 
+            catch (error) {
+                return null;  
+            }    
+        },
+        getEmployees: async function (oView) {
+            try {
+                const oRes = await fetch("/ojt/Employees?$expand=role,department",
+                {
+                    method: "GET",
+                    headers: {
+                        "Content-Type": "application/json" }
+                });
+                if ( oRes.status == 200 ) {
+                    let oresJson = await oRes.json();
+                    let empList = oresJson.value;
+                    this._setModel(oView, empList, "EmployeeList");
+                    return empList;
+                }
+            } 
+            catch (error) {
+                return null;  
+            }         
+        },
+
     };
 
 });
